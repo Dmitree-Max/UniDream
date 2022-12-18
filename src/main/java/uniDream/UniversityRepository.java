5b52eb3648adbff77dd6538b7a64c57434381861
Dmitree-Max <d.s.maximenko@gmail.com>
2022-12-19 02:57:14 +0300

    Implemented get universities request

---
+++ src/main/java/uniDream/UniversityRepository.java
@@ -0,0 +1,8 @@
+package uniDream;
+
+import uniDream.entities.University;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UniversityRepository extends CrudRepository<University, Integer> {
+
+}
